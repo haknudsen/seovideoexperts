80a5cdacad8bf26b6382da1ed6e5393607b00f28
Andy Knudsen <andy@websitetalkingheads.com>
2017-06-27 18:08:39 -0600

    Pre update

--- wp-content/themes/wp-bootstrap-starter/page-animation.php
+++ wp-content/themes/wp-bootstrap-starter/page-animation.php
@@ -20,8 +20,14 @@ get_header(); ?>
 	<div id="primary" class="content-area col-sm-12">
 		<main id="main" class="site-main" role="main">
 <h2 class="text-center">Animation Videos</h2>
-<p>Static commercials with images together with block letters are getting increasingly dated and less relevant. In this fast-paced society of online web videos, those that can utilize the advantages creating a website video is the ones who definitely will win. For the fresh generation of marketing professionals, this is actually where their strenght lies. Creative content creation—primarily by employing video production. </p>
-<p>At Talking Heads&#8482; we specialise in creating Animation videos that are truly is effective in data visualization and in depth data driven promotional campaigns while staying true with the short and made clear video making form of producing viral videos. Just look from our extensive index of companies in addition to organizations that have rooked our unique whiteboard explainer video trend. If you are not convinced, why not talk to our trusted representatives and get a quote. Make use of our services, engage in the new centuries and effortlessly enhance your web traffic with us. </p>
+<div class="row">
+    <div class="col-sm-6">
+        <p>At Talking Heads&#8482; we specialise in creating Animation videos that are truly is effective in data visualization and in depth data driven promotional campaigns while staying true with the short and made clear video making form of producing viral videos. Just look from our extensive index of companies in addition to organizations that have rooked our unique whiteboard explainer video trend. If you are not convinced, why not talk to our trusted representatives and get a quote. Make use of our services, engage in the new centuries and effortlessly enhance your web traffic with us. </p>
+    </div>
+    <div class="col-sm-6">
+        <p>Static commercials with images together with block letters are getting increasingly dated and less relevant. In this fast-paced society of online web videos, those that can utilize the advantages creating a website video is the ones who definitely will win. For the fresh generation of marketing professionals, this is actually where their strenght lies. Creative content creation—primarily by employing video production. </p>
+    </div>
+</div>
 
 <?php get_template_part( 'animation', 'index' ); ?>
 
